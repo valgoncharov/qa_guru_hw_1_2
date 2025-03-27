0ce1fa62e93101efe7d24cbd922c77780ec93015
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2025-03-27 19:51:39 +0300

    Change...

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def get_all_users() -> list[User]:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="localhost", port=8000, reload=True)
