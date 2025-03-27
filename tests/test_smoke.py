104234ec222f468a5b3196042b892b3ceadb88cb
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2025-03-27 19:59:00 +0300

    Deleted pytest fixture

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,15 +1,12 @@
-import pytest
 import requests
 from http import HTTPStatus
-from models.User import User
 
 
 class TestSmoke:
-    @pytest.mark.smoke('Smoke')
     def test_app_status(self, app_url: str):
-        response = requests.get(f"{app_url}/api/status")
+        response = requests.get(f"{app_url}/status")
         assert response.status_code == HTTPStatus.OK
 
     def test_smoke_users(self, app_url: str):
-        response = requests.get(f"{app_url}/api/users/")
+        response = requests.get(f"{app_url}/users/")
         assert response.status_code == HTTPStatus.OK
